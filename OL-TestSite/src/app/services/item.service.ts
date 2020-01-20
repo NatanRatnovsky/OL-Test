fb9f219760216e00f4abb78389a27494ab344cf4
NatanRatnovsky <nratnovsky@gmail.com>
2020-01-20 21:35:40 +0200

    Spring Boot App + Frond End Side

--- OL-TestSite/src/app/services/item.service.ts
+++ OL-TestSite/src/app/services/item.service.ts
@@ -17,22 +17,22 @@ export class ItemService {
   }
 
   public getAllItems(): Observable<Item[]> {
-    return this.httpClient.get<Item[]>('http://localhost:8080/api/allItems');
+    return this.httpClient.get<Item[]>('api/allItems');
   }
 
   public addItem(item: Item): Observable<Item> {
-    return this.httpClient.post<Item>('http://localhost:8080/api/addItem', item);
+    return this.httpClient.post<Item>('api/addItem', item);
   }
 
   public updateItem(item: Item): Observable<Item> {
-    return this.httpClient.put<Item>('http://localhost:8080/api/updateItem', item);
+    return this.httpClient.put<Item>('api/updateItem', item);
   }
 
   public deleteItem(id: number) {
-    return this.httpClient.delete('http://localhost:8080/api/rmItem/' + id, this.httpOptions);
+    return this.httpClient.delete('api/rmItem/' + id, this.httpOptions);
   }
 
   public getItem(id: number): Observable<Item> {
-    return this.httpClient.get<Item>('http://localhost:8080/api/item' + id);
+    return this.httpClient.get<Item>('api/item' + id);
   }
 }
